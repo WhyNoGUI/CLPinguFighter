c665e01117eb99e74a17b7b0a7640d071d5fd32c
Moritz-Friedemann <76700932+Moritz-Friedemann@users.noreply.github.com>
2020-12-29 16:12:41 +0100

    damage system rewritten

--- src/main/java/com/whynogui/clpf/Board.java
+++ src/main/java/com/whynogui/clpf/Board.java
@@ -39,7 +39,7 @@ public class Board {
             player2.facingRight = false;
         }
 
-        if (player1.getHitbox() != null && player1.getHitbox().intersects(player2.getHurtBox())) {
+        /*if (player1.getHitbox() != null && player1.getHitbox().intersects(player2.getHurtBox())) {
             if (player2.getState().equals("block")) {
 
             } else if (player2.getState().equals("invincible")) {
@@ -76,6 +76,110 @@ public class Board {
             player1.state = "neutral";
             player2.iceblock = null;
             hitCooldown();
+        }*/
+        
+        //Player 1 damage
+        switch (player1.getState()) {
+            case "heavyPunch" -> {
+                if (player1.getHitbox() != null && player1.getHitbox().intersects(player2.getHurtBox())) {
+                    if (player2.getState().equals("block")) {
+
+                    } else if (player2.getState().equals("invincible")) {
+                    } else {
+                        {
+                            player2.health -= Player.HEAVY_PUNCH_DAMAGE;
+                            player2.state = "invincible";
+                            player2.cooldown = 10;
+                            hitCooldown();
+                        }
+                    }
+                }
+            }
+            case "lightPunch" -> {
+                if (player1.getHitbox() != null && player1.getHitbox().intersects(player2.getHurtBox())) {
+                    if (player2.getState().equals("block")) {
+
+                    } else if (player2.getState().equals("invincible")) {
+                    } else {
+                        {
+                            player2.health -= Player.LIGHT_PUNCH_DAMAGE;
+                            player2.state = "invincible";
+                            player2.cooldown = 10;
+                            hitCooldown();
+                        }
+                    }
+                }
+            }
+            case "uppercut" -> {
+                if (player1.getHitbox() != null && player1.getHitbox().intersects(player2.getHurtBox())) {
+                    player2.health -= Player.UPPERCUT_DAMAGE;
+                    player2.state = "invincible";
+                    player2.cooldown = 10;
+                    hitCooldown();
+                    player1.state = "jump";
+                }
+            }
+            case "special" -> {
+                if (player1.getIceblock() != null && player1.getIceblock().intersects(player2.getHurtBox())) {
+                    player2.health -= Player.ICE_BLOCK_DAMAGE;
+                    player2.state = "invincible";
+                    player2.cooldown = 10;
+                    player1.iceblock = null;
+                    hitCooldown();
+                }
+            }
+        }
+        
+        //Player 2 damage
+        switch (player2.getState()) {
+            case "heavyPunch" -> {
+                if (player2.getHitbox() != null && player2.getHitbox().intersects(player1.getHurtBox())) {
+                    if (player1.getState().equals("block")) {
+
+                    } else if (player1.getState().equals("invincible")) {
+                    } else {
+                        {
+                            player1.health -= Player.HEAVY_PUNCH_DAMAGE;
+                            player1.state = "invincible";
+                            player1.cooldown = 10;
+                            hitCooldown();
+                        }
+                    }
+                }
+            }
+            case "lightPunch" -> {
+                if (player2.getHitbox() != null && player2.getHitbox().intersects(player1.getHurtBox())) {
+                    if (player1.getState().equals("block")) {
+
+                    } else if (player1.getState().equals("invincible")) {
+                    } else {
+                        {
+                            player1.health -= Player.LIGHT_PUNCH_DAMAGE;
+                            player1.state = "invincible";
+                            player1.cooldown = 10;
+                            hitCooldown();
+                        }
+                    }
+                }
+            }
+            case "uppercut" -> {
+                if (player2.getHitbox() != null && player2.getHitbox().intersects(player1.getHurtBox())) {
+                    player1.health -= Player.UPPERCUT_DAMAGE;
+                    player1.state = "invincible";
+                    player1.cooldown = 10;
+                    hitCooldown();
+                    player2.state = "jump";
+                }
+            }
+            case "special" -> {
+                if (player2.getIceblock() != null && player2.getIceblock().intersects(player1.getHurtBox())) {
+                    player1.health -= Player.ICE_BLOCK_DAMAGE;
+                    player1.state = "invincible";
+                    player1.cooldown = 10;
+                    player2.iceblock = null;
+                    hitCooldown();
+                }
+            }
         }
 
         if (player1.health <= 0) {
